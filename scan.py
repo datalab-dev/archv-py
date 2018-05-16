e0befffed802aecaa0b044d97fcdd410d2d82941
avkoehl <avkoehl@ucdavis.edu>
2018-05-15 19:07:44 -0700

    scan bug fixed, occured if less than 2 keypoints, need to validate the results and time now

--- scan.py
+++ scan.py
@@ -9,42 +9,48 @@ from filter import ratio_test, symmetry_test, ransac_test, filter_keypoints
 from utils import read_from_file
 
 
+### parameters
 input = "seed.jpg"
 imagedir = "/home/arthur/imageset/sample/"
 keypointdir = "./output/keypoints/"
 filenames = glob.glob(os.path.join(keypointdir, "*.yml"))
-outputdir = "./output/matches/"
+outputfile = "matches_" + input.split('.')[0] + ".txt"
+ofile = open(outputfile, "w")
 
-#
+### process input image
 img = cv2.imread(input,0) # trainImage
 surf = cv2.xfeatures2d.SURF_create(3000, 10, 10)
 keys = surf.detect(img, None)
 keys1 = filter_keypoints (keys, 50, 500)
 temp, des1 = surf.compute(img, keys1)
 
+### process all the keypoint files
 filenames = glob.glob(os.path.join("./output/keypoints/", "*.yml"))
+scores = []
 for i,fname in enumerate(filenames):
-    print ("processing image ", i,  filenames[i].split('/')[-1], " out of ", len(filenames) - 1)
+
+    if (i % 50 == 0):
+        print ("processing image ", i,  " out of ", len(filenames) - 1)
 
     #read in keypoints and descriptors for each image
     keys2, des2 = read_from_file (fname)
-    print (len(keys2), len(des2))
+
+    if (len(keys2) < 2):
+        continue
 
     # find matches
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2, k=2)
+    matches = bf.knnMatch(des1, des2, k=2)
     matches2 = bf.knnMatch(des2, des1, k=2)
     
     # apply tests
-    print (len(matches), len(matches2))
     good = ratio_test(matches)
     good2 = ratio_test(matches2)
     sym = symmetry_test(good, good2)
     ransac = ransac_test (keys1, keys2, sym) 
 
-    # output results
-    if len(ransac) >= 5:
-        print ("num matches: ", len(matches))
-        print ("post ratio test: ", len(good))
-        print ("post symmetry test: ", len(sym))
-        print ("post ransac test: ", len(ransac))
+    # store results
+    scores.append(fname.split('/')[-1] + " " + str(len(ransac)))
+
+for s in scores:
+    print (s, file=ofile)
