be7a0a3271d676d08af89a594f70d7061e8de6ad
avkoehl <avkoehl@ucdavis.edu>
2018-05-16 14:26:43 -0700

    refactor, command line arguments to be added next

--- show_keypoints.py
+++ show_keypoints.py
@@ -1,20 +1,38 @@
 import cv2
 import numpy as np
 from filter import filter_keypoints
- 
-img = cv2.imread("./images/examples/test.jpg", cv2.IMREAD_GRAYSCALE)
 
-#minh, noctaves, nlayers
-surf = cv2.xfeatures2d.SURF_create(5000, 8, 8) #Opencv 3+
- 
-# split into two steps to put filter_keypoints
-#keypoints, descriptors = surf.detectAndCompute(img, None)
-keys = surf.detect(img, None)
-keypoints = filter_keypoints(keys, 50, 500)
-keypoints, descriptors = surf.compute(img, keypoints)
 
-img = cv2.drawKeypoints(img, keypoints, None, (255,0,0),4)
+## configure, to be read in as command line arguments:
+minh = 5000
+octaves = 8
+layers = 8
+msize = 50
+mresponse = 500 
+image = "./images/examples/test.jpg" 
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+
+
+
+def main():
+
+    img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+
+    #minh, noctaves, nlayers
+    surf = cv2.xfeatures2d.SURF_create(minh, octaves, layers) #Opencv 3+
+
+    keys = surf.detect(img, None)
+    keypoints = filter_keypoints(keys, msize, mresponse)
+    keypoints, descriptors = surf.compute(img, keypoints)
+
+    img = cv2.drawKeypoints(img, keypoints, None, (255,0,0),4)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
