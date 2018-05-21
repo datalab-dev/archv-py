aa0028a0d7a6f6eee17ba827e7a23440fe06b499
avkoehl <avkoehl@ucdavis.edu>
2018-05-21 16:41:05 -0700

    showkeypoints arguments are now optional, wiht defaults, and can alternatively read in a keypointfile

--- show_keypoints.py
+++ show_keypoints.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
-Usage: python show_keypoints.py 2000 8 8 50 500 /path/to/image
+Usage: python show_keypoints.py /path/to/image -minh 2000 -o 8 -l 8 -s 50 -r 500 
+   or: python show_keypoints.py /path/to/image -k /path/to/keypointfile
 
 A simple script that is used for tuning the surf parameters used by archv for the imageset. 
 This is important becuase each imageset has generally different levels of detail and can require very different
@@ -11,9 +12,10 @@ surf parameters in order to have a good balance between many features and run ti
 
 import numpy as np
 import cv2
+import sys
 import argparse
 from filter import filter_keypoints
-from utils import compute_and_filter
+from utils import compute_and_filter, read_from_file
 
 def parse_arguments ():
     """ 
@@ -21,18 +23,17 @@ def parse_arguments ():
     returns dictionary of arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("minh", help="Set the threshold for minhessian",
-            type=int)
-    parser.add_argument("octaves", help="Set the number of octaves of scale space for the image",
-            type=int)
-    parser.add_argument("layers", help="Set the number of octave layers",
-            type=int)
-    parser.add_argument("msize", help="Set the minimum size of keypoints",
-            type=int)
-    parser.add_argument("mresponse", help="Set the minimum response of keypoints",
-            type=float)
-    parser.add_argument("image", help="path to input image file",
-            type=str)
+
+    parser.add_argument("image", help="path to input image file", type=str)
+
+    parser.add_argument("-k", help="path to yml file containing keypoints and descriptors", type=str, default=None)
+
+    parser.add_argument("-minh", help="Set the threshold for minhessian", type=int, default=2000)
+    parser.add_argument("-o", help="Set the number of octaves of scale space for the image", type=int, default=8)
+    parser.add_argument("-l", help="Set the number of octave layers", type=int, default=8)
+    parser.add_argument("-s", help="Set the minimum size of keypoints", type=int, default=50)
+    parser.add_argument("-r", help="Set the minimum response of keypoints", default=500.0)
+
     args = parser.parse_args()
     return args
 
@@ -43,10 +44,13 @@ def main(args):
     arguments are passed in from the parse_arguments function. order for arguments:
     show_keypoints.py minh octaves layers mszie mresponse path/to/image
     """ 
-
     img = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
 
-    keypoints, descriptors = compute_and_filter (img, args.minh, args.octaves, args.layers, args.msize, args.mresponse)
+    if args.k:
+        keypoints, descriptors = read_from_file(args.k)
+
+    else:
+        keypoints, descriptors = compute_and_filter (img, args.minh, args.o, args.l, args.s, args.r)
 
 
     img = cv2.drawKeypoints(img, keypoints, None, (255,0,0),4)
