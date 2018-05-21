338d43050384ae2b792e5a6e587b6c3efc28909f
avkoehl <avkoehl@ucdavis.edu>
2018-05-21 08:47:25 -0700

    compute and filter function added

--- show_keypoints.py
+++ show_keypoints.py
@@ -1,5 +1,6 @@
-import cv2
 import numpy as np
+import cv2
+import argparse
 from filter import filter_keypoints
 
 
@@ -14,17 +15,21 @@ image = "./images/examples/test.jpg"
 
 
 
+def compute_and_filter (img, minh, octaves, layers, msize, mresponse):
+    surf = cv2.xfeatures2d.SURF_create(minh, octaves, layers) #Opencv 3+
+    keys = surf.detect(img, None)
+    keypoints = filter_keypoints(keys, msize, mresponse)
+
+    return surf.compute(img, keypoints)
+
+
 
 def main():
 
     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
-    #minh, noctaves, nlayers
-    surf = cv2.xfeatures2d.SURF_create(minh, octaves, layers) #Opencv 3+
+    keypoints, descriptors = compute_and_filter (img, minh, octaves, layers, msize, mresponse)
 
-    keys = surf.detect(img, None)
-    keypoints = filter_keypoints(keys, msize, mresponse)
-    keypoints, descriptors = surf.compute(img, keypoints)
 
     img = cv2.drawKeypoints(img, keypoints, None, (255,0,0),4)
 
