bc4b4f9b314d844962d2a05eee2ca67e4e1704fb
avkoehl <avkoehl@ucdavis.edu>
2018-05-22 14:27:23 -0700

    scan reworked, main untested

--- archv/main.py
+++ archv/main.py
@@ -18,26 +18,27 @@ def parse_arguments():
     """ Simple parser for command line arguments """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("image", help="path to image", type=str)
-    parser.add_argument("-k", required=True, help="path to keypoint files", type=str)
+    parser.add_argument("-i", required=True, help="path to input image directory", type=str)
+    parser.add_argument("-k", required=True, help="path to keypoints for image directory", type=str)
     parser.add_argument("-p", required=True, help="path to parameter file for surf", type=str)
+    parser.add_argument("-o", default="results.csv", help="path to outputfile", type=str)
+    parser.add_argument("-n", default=1, help="number of cores", type=int)
 
     args = parser.parse_args()
     return args
 
-def main(args):
-    scores = []
-    names = []
-    results = []
+def find_matches(fname, params):
 
-    img1 = Image(args.image)
-    params = yaml.load(open(args.p))
+    img1 = Image(fname)
     img1.compute_and_filter(params["min_hessian"], params["octaves"], params["layers"], params["min_size"], params["min_response"])
 
-    filenames = glob.glob(os.path.join(args.k, "*.yml"))
-    for fname in filenames:
+    scores = []
+    names = []
+
+    kyptfiles = glob.glob(os.path.join(args.k, "*.yml"))
+    for keys in kyptfiles:
         img2 = Image(None)
-        img2.read_from_file(fname)
+        img2.read_from_file(keys)
 
         if (len(img2.keypoints) < 2):
             continue
@@ -50,10 +51,9 @@ def main(args):
         score = len(matcher.ransac_matches)
 
         if score > 0:
-            names.append(fname.split('/')[-1].split('.')[0])
+            names.append(keys.split('/')[-1].split('.')[0])
             scores.append(score)
 
-    #sort the results
     scores = np.array(scores)
     names = np.array(names)
     indexs = scores.argsort()
@@ -63,12 +63,26 @@ def main(args):
     for i in range(len(names)):
         matches.insert(0, names[i] + " " + str(scores[i]))
     results = ",".join(matches)
-    print (results)
+
+    return results
+
+def main(args):
+    params = yaml.load(open(args.p))
+    filenames = glob.glob(os.path.join(args.i, "*.jpg"))
+
+    full = []
+    ofile = open(args.o, "w")
+
+    full = Parallel(n_jobs=args.n)(delayed(find_matches)(fname, params) for fname in filenames)
+
+    print (full, file=ofile)
+
+
 
 if __name__ == "__main__":
     start = time.time()
     args = parse_arguments()
-    results = main(args)
+    main(args)
     print ("Time Elapsed: ", time.time() - start)
 
 
