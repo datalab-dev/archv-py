24f545fb8e3ba46c1024a4be478111df784012a4
Arthur Koehl <arthurkoehl@Arthurs-MacBook-Pro.local>
2018-06-27 12:41:14 -0700

    only need to maintain requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='archv',
     version='0.1.0',
     packages=['archv', 'scripts'],
     long_description=open('README.txt').read(),
-    install_requires=['scipy', 'pyyaml', 'opencv-contrib-python', 'opencv-python'],
+    install_requires=requirements,
 )
