91569ac35b760e5d38029263daed697b5fc5543e
Arthur Koehl <avkoehl@ucdavis.edu>
2021-08-05 16:46:50 -0700

    docstring for image class

--- archv/image.py
+++ archv/image.py
@@ -1,10 +1,39 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import cv2
 
 class Image():
+    """ 
+    A class to represent an image. Handles loading the image from file
+    as an opencv image, as well as provides functions for computing
+    keypoints + descriptors for the image. Can save and load keypoints
+    and descriptors from yaml file.
+
+    Attributes
+    ----------
+    name: str
+        image filename
+    keypoints: list
+        list of keypoint vectors for the image
+    descriptors: list
+        list of descriptor vectors for the image
+    minh: int 
+    minr: float
+    mins: int
+    octaves: int
+    layers: int
+
+    Methods
+    -------
+    filter_keypoints(mins, minr):
+        filters keypoints that are smaller than mins or minr
+    computer_and_filter(minh, octaves, layers, mins, minr):
+       computes and filters keypoints for an image based on SURF parameters
+    read_from_file(ifile):
+       reads keypoints and descriptors for an image from yaml file
+    write_to_file(oftile):
+       writes keypoints and descriptors for an image to yaml file
+    
+    """
 
     def __init__(self, image):
         self.image = None
